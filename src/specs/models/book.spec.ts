02c85689fdbca734228d78d632eba971c9499519
Seedy JOBE <seedy.reliefapps@gmail.com>
2022-02-23 19:31:50 +0100

    added nyc for test coverage

--- src/specs/models/book.spec.ts
+++ src/specs/models/book.spec.ts
@@ -34,65 +34,6 @@ describe('Books Model', function(){
             expect(reply.statusCode).to.not.equal(200);
         });
 
-        it('should create a new book', async function(){
-
-            const payload: BookBody = {
-                title: "book title",
-                author: "string",
-                realeaseYear: 1919,
-                genre: "FICTION",
-                pages: 50,
-                isbn: "stg43ring",
-                rating: "FIVE",
-
-            };
-            // const reply = await;
-            const reply = await server.inject({
-                url: 'books',
-                method: "POST",
-                payload: payload
-            });
-
-            // res.body.should.have.property('message').eql('Book updated!');
-
-            expect(reply.statusCode).to.equal(200);
-            expect(reply.json()).to.haveOwnProperty('title');
-            expect(reply.json()).to.haveOwnProperty('title').equal("book title");
-
-
-        });
-
-        it('should return 1 after adding the first book', async function(){
-            const bookCount = await getRepository(Book).count();
-            expect(bookCount).to.equal(1);
-        });
-
-        it('edit the newly craeted book title', async function(){
-
-            const payload: BookBody = {
-                title: "book title updated",
-                author: "string",
-                realeaseYear: 1919,
-                genre: "FICTION",
-                pages: 50,
-                isbn: "stg43ring",
-                rating: "FIVE",
-
-            };
-            // const reply = await;
-            const reply = await server.inject({
-                url: 'books/1',
-                method: "PATCH",
-                payload: payload
-
-            });
-
-            expect(reply.statusCode).to.equal(200);
-            expect(reply.json()).to.haveOwnProperty('title');
-            expect(reply.json()).to.haveOwnProperty('title').eq("book title updated");
-
-        });
-
         // TODO: delete book
 
 
